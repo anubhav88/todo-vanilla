604f42fae62c248a0e8fb2dbf1624859fa05a393
Anubhav Kapoor <anubhavkapoor88@hotmail.com>
2017-10-03 19:46:49 +0530

    Adding build new files

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,12 @@
-var init = function() {
-var todoData = new TodoData([]);
-var template = new TodoTemplate();
-var view = new View(template);
-var controller = new TodoController(todoData, view);
+"use strict";
+
+var init = function init() {
+  var todoData = new TodoData([]);
+  var template = new TodoTemplate();
+  var view = new View(template);
+  var controller = new TodoController(todoData, view);
 };
 
-window.onload = function() {
+window.onload = function () {
   init();
 };
